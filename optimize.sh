4d14657e88e9e6d77175414587cd58bd4d9292b6
bixamon <bixamon@users.noreply.github.com>
2020-12-13 18:21:56 +0000

    Automated deployment: Sun Dec 13 18:21:56 UTC 2020 6e00325557ef29b9974d7100f99f1a2c0a56461e

--- optimize.sh
+++
@@ -1,6 +0,0 @@
-for file in $(find public -name "*.html"); do
-    [ -f "$file" ] || break
-    echo "Optimizing $file"
-    ! $GOPATH/bin/transform $file > /tmp/optimized.txt
-    [ -s /tmp/optimized.txt ] && cat /tmp/optimized.txt > $file || echo "Not a valid AMP page. Omitting..."
-done
